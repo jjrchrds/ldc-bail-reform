554e845f5e93c81c99b3e3a31f44a7baca0c8d2b
Maggie <mc@maggiecheung.ca>
2020-10-01 11:29:30 -0400

    Linked contentful up to Issues page

--- src/pages/issue1.js
+++ src/pages/issue1.js
@@ -1,6 +1,7 @@
 import React from "react"
 import { Row, Col } from "react-bootstrap"
 import { useStaticQuery, graphql } from "gatsby"
+import { documentToHTMLString, documentToReactComponents } from "@contentful/rich-text-react-renderer"
 
 import Layout from "../components/layout"
 import Head from '../components/head'
@@ -62,17 +63,31 @@ const Issue1Page = () => {
   `)
 
   const issueContents = data.issueContent.edges[0].node;
-  console.log(issueContents.issueName);
+
+  // This splits our list of sources in half so we can list them in two columns
+  const allSources = issueContents.sources
+  const sourcesLeft = allSources.slice(0, Math.ceil(allSources.length / 2))
+  const sourcesRight = allSources.slice(Math.ceil(allSources.length / 2), allSources.length)
+  console.log(sourcesLeft)
 
   return (
     <Layout>
       <Head title="Issues"/>
-      <IssuesHero issueName={ issueContents.issueName } issueImg={ data.issue1.childImageSharp.fluid } link1="/issue1#momentum" link2="/issue1#opportunity" link3="/issue1#explanation" />
+      <IssuesHero 
+        issueName={ issueContents.issueName } 
+        issueImg={ data.issue1.childImageSharp.fluid }
+        issueBlurb={ issueContents.issueBlurb }
+        heroMomentum={ issueContents.heroMomentum.heroMomentum }
+        heroOpportunity={ issueContents.heroOpportunity.heroOpportunity }
+        heroEE={ issueContents.heroEE.heroEE }
+      />
 
       <Row id="momentum" className="justify-content-center mx-0 pt-5 bg-light">
         <Col className="mb-2" xs="11" lg="10" xl="7">
           <h2 className="text-center uppercase">Momentum</h2>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris feugiat orci vel justo semper varius. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Nam fermentum, massa ut molestie venenatis, justo nibh laoreet risus, at imperdiet nisl purus sed augue.</p>
+          {documentToReactComponents(
+            issueContents.momentumBlurb.json
+          )}
         </Col>
       </Row>
 
@@ -85,7 +100,9 @@ const Issue1Page = () => {
       <Row id="opportunity" className="justify-content-center mx-0 pt-5 pb-5 pl-4 pr-4 bg-rust">
         <Col className="mb-2 pt-4 pb-4 crooked-box"  xs="11" lg="10" xl="7">
           <h2 className="text-center uppercase text-pink">Opportunity</h2>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris feugiat orci vel justo semper varius. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Nam fermentum, massa ut molestie venenatis, justo nibh laoreet risus, at imperdiet nisl purus sed augue.</p>
+          {documentToReactComponents(
+            issueContents.opportunityBlurb.json
+          )}
         </Col>
       </Row>
 
@@ -101,24 +118,25 @@ const Issue1Page = () => {
           <Row className="mt-4 pt-5 pb-3 crooked-box">
             <Col md="12" lg="6">
               <ul>
-                <li className="pb-4"><p>A recent study of supervised bail programs showed that over 40% of the group had mental health issues, 31% had recurring mental health and substance abuse issues, while one-third of this group identified themselves as homeless.</p>
-                (<a href="https://example.com">A Legal Aid Strategy for Bail, Legal Aid Ontario</a>)</li>
-
-                <li className="pb-4"><p>Black people are overrepresented in federal prisons by more than 300%, while Aboriginal people are overrepresented by nearly 500%. The same disparities exist in provincial jails.</p>
-                (<a href="https://example.com">Race, Crime and Justice in Canada, John Howard Society of Canada</a>)</li>
-
-                <li className="pb-4"><p>40% of police work involves people in crisis or people experiencing a mental illness.</p>
-                (<a href="https://example.com">Study in Blue and Grey, Canadian Mental Health Association - BC Division</a>)</li>
+                {sourcesLeft.map(item => (
+                  <li>
+                    {documentToReactComponents(
+                      item.fact.json
+                    )}
+                  </li>
+                ))}
               </ul>
             </Col>
 
             <Col md="12" lg="6">
               <ul>
-                <li className="pb-4"><p>The estimates of untreated mental illness in the criminal justice system range from 15-40% of the incarcerated population.</p>
-                (<a href="https://example.com">Mental Illness and Police Fact Sheets, Canadian Mental Health Association - BC Division</a>)</li>
-
-                <li className="pb-4"><p>43% of persons remanded into custody were unemployed. In 2000, the National Council of Welfare concluded, “the reason that accused persons are kept in custody for weeks and months before trial is not because they are dangerous and poor, but mostly because they are poor.”</p>
-                (<a href="https://example.com">A Legal Aid Strategy for Bail, Legal Aid Ontario</a>)</li>
+                {sourcesRight.map(item => (
+                  <li>
+                    {documentToReactComponents(
+                      item.fact.json
+                    )}
+                  </li>
+                ))}
               </ul>
             </Col>
           </Row>
