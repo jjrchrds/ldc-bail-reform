961dec477934844c0b068142493e8c7c1f8f0e02
Maggie <mc@maggiecheung.ca>
2020-10-15 17:58:24 -0400

    Correcting GA ID

--- src/components/head.js
+++ src/components/head.js
@@ -22,7 +22,7 @@ const Head = ({title}) => {
         innerHTML: 'window.__lo_site_id = 253250;(function(){var wa = document.createElement("script"); wa.type = "text/javascript"; wa.async = true; wa.src = "https://d10lpsik1i8c69.cloudfront.net/w.js"; var s = document.getElementsByTagName("script")[0]; s.parentNode.insertBefore(wa, s);})();'
       }]}
     >
-    {ReactGA.initialize("UA-180594039")}
+    {ReactGA.initialize("UA-180594039-1")}
     </Helmet>
   )
 }
