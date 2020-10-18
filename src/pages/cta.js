ff82cdd2fb237bc83ebe292426b3de1873b7a28e
Maggie <mc@maggiecheung.ca>
2020-10-17 21:21:37 -0400

    Created CTA page

---
+++ src/pages/cta.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { Link } from "gatsby"
+
+import Layout from "../components/layout";
+import Head from '../components/head';
+
+const CTA = () => {
+  return (
+    <Layout>
+      <Head title="Get in Touch"/>
+      <h1>Get in Touch</h1>
+      <p>View info.</p>
+    </Layout>
+  )
+}
+
+export default CTA;
