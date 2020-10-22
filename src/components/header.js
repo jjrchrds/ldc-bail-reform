94d164ef52c5fcc0c3fcf7241d44c3b5cf225a7b
Maggie <mc@maggiecheung.ca>
2020-10-22 17:56:33 -0400

    Updated navbar: "Timeline" & "Themes"

--- src/components/header.js
+++ src/components/header.js
@@ -18,12 +18,12 @@ class Header extends React.Component {
             <Nav className="ml-auto">
               <Nav.Link as={Link} activeClassName="active" to="/narrative">Stories</Nav.Link>
               <Nav.Link as={Link} activeClassName="active" to="/system-map">System Map</Nav.Link>
-              <NavDropdown title="Issues" className="nav-dropdown" variant="dark" activeClassName="active">
+              <NavDropdown title="Themes" className="nav-dropdown" variant="dark" activeClassName="active">
                 <NavDropdown.Item href="/issue1">Damaging Lives</NavDropdown.Item>
                 <NavDropdown.Item href="/issue2">Denying Dignity</NavDropdown.Item>
                 <NavDropdown.Item href="/issue3">Culture of Fear</NavDropdown.Item>
               </NavDropdown>
-              <Nav.Link as={Link} activeClassName="active" to="/methodology">Methodology</Nav.Link>
+              <Nav.Link as={Link} activeClassName="active" to="/methodology">Timeline</Nav.Link>
               <Nav.Link as={Link} to="/cta" className={headerStyles.ctaHover}><p className={headerStyles.cta}>About the Beta</p></Nav.Link>
             </Nav>
           </Navbar.Collapse>
