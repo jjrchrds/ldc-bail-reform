b9366c28d62d425661c308c2400c7b3d9857cfe3
e-wai <e.wai.7155@gmail.com>
2020-10-22 21:34:45 -0400

    Change dimension on header from width to height

--- src/components/header.js
+++ src/components/header.js
@@ -12,7 +12,7 @@ const Header = () => {
     <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark" fixed="top">
       <Container>
         <Navbar.Brand as={Link} to="/" aria-label="Go to homepage">
-          <Logo width={200}/>
+          <Logo height={35}/>
         </Navbar.Brand>
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
         <Navbar.Collapse id="responsive-navbar-nav" className={headerStyles.nav}>
