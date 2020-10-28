0d894e45afc87f567c07c6d2faa8f4cd95f86771
Maggie <mc@maggiecheung.ca>
2020-10-27 20:44:01 -0400

    Connected Contentful GraphQL query to beta page

--- src/pages/cta.js
+++ src/pages/cta.js
@@ -1,16 +1,82 @@
 import React from "react";
-import { Container } from "react-bootstrap"
+import { Container } from "react-bootstrap";
+import { useStaticQuery, graphql } from "gatsby";
 
 import Layout from "../components/layout";
 import Head from '../components/head';
 
 const CTA = () => {
+  const data = useStaticQuery(graphql`
+    query {
+      betaContent: allContentfulAboutTheBetaTemplate {
+        nodes {
+          title
+          introText {
+            json
+          }
+          section2Title
+          section2Subtitle
+          section2Content1 {
+            json
+          }
+          section2Content2 {
+            json
+          }
+          section3Title
+          collage1text {
+            json
+          }
+          collage1 {
+            fluid(quality:100, maxWidth:1000) {
+              src
+            }
+          }
+          dualCollageTitle
+          collage2 {
+            fluid(quality:100, maxWidth:1000) {
+              src
+            }
+          }
+          collage2text {
+            json
+          }
+          collage3 {
+            fluid(quality:100, maxWidth:1000) {
+              src
+            }
+          }
+          collage3text {
+            json
+          }
+          section4Title1
+          section4Part1 {
+            json
+          }
+          section4BigTitle
+          section4Title2
+          section4Part2 {
+            json
+          }
+          section4Title3
+          section4Part3 {
+            json
+          }
+          section4Part4 {
+            json
+          }
+        }
+      }
+    }
+  `)
+  
+  const content = data.betaContent.nodes[0];
+  
   return (
     <Layout>
-      <Head title="Get in Touch"/>
+      <Head title="About the Beta"/>
 
       <Container className="mt-5">
-        <h1 className="text-center">Get in Touch</h1>
+        <h1 className="text-center">{content.title}</h1>
         <p className="text-center">Integer posuere erat a ante venenatis dapibus posuere velit aliquet.</p>
       </Container>
     </Layout>
