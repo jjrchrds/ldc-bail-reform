e6a15dccd07b1175f29e5bd3a555b3391425848d
Joshua Richards <joshua.j.richards@gmail.com>
2020-10-28 23:05:04 -0400

    update stories and nathan

---
+++ src/components/stories/rich-text.js
@@ -0,0 +1,28 @@
+import React from "react"
+import { BLOCKS, MARKS } from "@contentful/rich-text-types"
+import { documentToReactComponents } from "@contentful/rich-text-react-renderer"
+
+const RichText = ({json}) => {
+
+  console.log(json);
+
+  const Bold = ({ children }) => <span className="bold">{children}</span>
+  const Text = ({ children }) => <p className="align-center">{children}</p>
+  
+  const options = {
+    renderMark: {
+      [MARKS.BOLD]: text => <Bold>{text}</Bold>,
+    },
+    renderNode: {
+      [BLOCKS.PARAGRAPH]: (node, children) => <Text>{children}</Text>,
+    },
+  }
+
+  return (
+    <>
+    {documentToReactComponents( json , options) }
+    </>
+  )
+}
+
+export default RichText
